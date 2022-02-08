84e2be60925ee9c246d4d91a840dcb86d262cf87
GENZxM <97391354+GENZxM@users.noreply.github.com>
2022-02-08 17:48:10 +0800

    Update __main__.py

--- JisooX/__main__.py
+++ JisooX/__main__.py
@@ -48,7 +48,7 @@ _Maintained by_ [{}](tg://user?id={})
 
 
 HELP_STRINGS = """
-Hey there! My name is *Dion Robot*.
+Hey there! My name is *XFlickzRobot*.
 I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
@@ -73,7 +73,7 @@ def vercheck() -> str:
 
 SOURCE_STRING = """
 ⚡I'm built in python3, using the python-telegram-bot library, and am fully opensource - you can find what makes me tick [here](https://t.me/seorangdion)
-⚡You Can Clone Me [Here](https://heroku.com/deploy?template=https://t.me/seorangdion)
+⚡You Can Clone Me [Here](https://heroku.com/deploy?template=https://t.me/XFlickz)
 """
 
 
@@ -92,7 +92,7 @@ GDPR = []
 
 START_IMG = os.environ.get('START_IMG', None)
 if START_IMG is None:
-    img = "https://telegra.ph/file/386d65041ecd05dec81b4.jpg"
+    img = "https://telegra.ph/file/a30bd9836f7d9a0ed0651.jpg"
 else:
   img = START_IMG    
     
@@ -193,8 +193,8 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="[► Help ◄]",callback_data="help_back"),InlineKeyboardButton(text="[► Creator ◄]",url="https://t.me/seorangdion")]]
-    keyboard += [[InlineKeyboardButton(text="[► Source ◄]",url="https://github.com/SeorangDion"),InlineKeyboardButton(text="[► Add Me ◄]",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard = [[InlineKeyboardButton(text="[► Help ◄]",callback_data="help_back"),InlineKeyboardButton(text="[► Developer ◄]",url="https://t.me/XFlick")]]
+    keyboard += [[InlineKeyboardButton(text="[► Source ◄]",url="https://github.com/GENZxM"),InlineKeyboardButton(text="[► Add Me ◄]",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
@@ -295,7 +295,7 @@ def get_help(bot: Bot, update: Update):
         update.effective_message.reply_text("Contact me in PM to get the list of possible commands.",
                                             reply_markup=InlineKeyboardMarkup(
                                                 [[InlineKeyboardButton(text="[► Help ◄]",url="t.me/{}?start=help".format(bot.username))],  
-                                                [InlineKeyboardButton(text="[► Creator ◄]",url="https://t.me/seorangdion")]]))
+                                                [InlineKeyboardButton(text="[► Developer ◄]",url="https://t.me/XFlick")]]))
         return
 
     elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
